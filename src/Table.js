d6195a493c1bfba3cf19b0e46979137f84012cf3
KennaReed <mreed16@calpoly.edu>
2021-01-08 11:35:04 -0800

    First Commit of my react app

---
+++ src/Table.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './index.css'
+
+function TableHeader() {
+    return (
+        <thead className='tableHeader'>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+
+function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        );
+    });
+    
+    return (
+        <tbody className='tableBody'>
+            {rows}
+        </tbody>
+    );
+}
+
+function Table(props) { 
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={props.characterData}/>
+        </table>
+    );
+}
+
+export default Table;
