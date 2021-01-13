ecf380c13988e20304171fd9b24083c0253d3569
KennaReed <mreed16@calpoly.edu>
2021-01-13 11:52:56 -0800

    Linked Frontend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,27 +1,48 @@
-import React, {useState} from 'react'
-import Table from './Table'
-import Form from './Form'
+import React, {useState, useEffect} from 'react';
+import Table from './Table';
+import Form from './Form';
+import axios from 'axios';
+import { fireEvent } from '@testing-library/react';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-function removeOneCharacter (index) {
-  const updated = characters.filter((character, i) => {
-    return i !== index
-  });
-  setCharacters(updated)
-}
+  useEffect(() => {
+    fetchAll().then(result => {
+      if (result.status === 200)
+        setCharacters(result);
+    });
+  }, []);
 
-function updateList(person) {
-  setCharacters([...characters, person]);
-}
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated)
+  }
 
-return (
-  <div className="table">
-      <Table characterData={characters} removeCharacter={removeOneCharacter}/>
-      <Form handleSubmit={updateList}/>
-  </div>
-  );
+  async function fetchAll() {
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    }
+    
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  return (
+    <div className="table">
+        <Table characterData={characters} removeCharacter={removeOneCharacter}/>
+        <Form handleSubmit={updateList}/>
+    </div>
+    );
 }
 
 export default MyApp;
